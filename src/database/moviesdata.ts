bedbe0173b8cfee6c31acd31c8c5537b131c272c
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-07-24 06:18:15 -0500

    Create database

---
+++ src/database/moviesdata.ts
@@ -0,0 +1,63 @@
+import { Movie } from "../models/movies";
+export const moviesData: Movie[] = [
+    {
+        id: 1,
+        title: "The Shawshank Redemption",
+        director: "Frank Darabont",
+        year: 1994
+    },
+    {
+        id: 2,
+        title: "The Godfather",
+        director: "Francis Ford Coppola",
+        year: 1972
+    },
+    {
+        id: 3,
+        title: "The Dark Knight",
+        director: "Christopher Nolan",
+        year: 2008
+    },
+    {
+        id: 4,
+        title: "12 Angry",
+        director: "Sidney Lumet",
+        year: 1957
+    },
+    {
+        id:5,
+        title: "Back to the Future",
+        director: "Robert Zemeckis",
+        year: 1985
+    },
+    {
+        id: 6,
+        title: "The Lord of the Rings: The Return of the King",
+        director: "Peter Jackson",
+        year: 2003
+    },
+    {
+        id: 7,
+        title: "The Lion King",
+        director: "Roger Allers, Rob Minkoff",
+        year: 1994
+    },
+    {
+        id: 8,
+        title: "The Matrix",
+        director: "Lana Wachowski, Lilly Wachowski",
+        year: 1999
+    },
+    {
+        id: 9,
+        title: "Inception",
+        director: "Christopher Nolan",
+        year: 2010
+    },
+    {
+        id: 10,
+        title: "The Departed",
+        director: "Martin Scorsese",
+        year: 2006
+    }
+];
